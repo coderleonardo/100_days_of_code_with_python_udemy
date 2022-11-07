a64ac2edd82086d3caffc2de45ca2f2c31d90004
Leonardo Cesar <coderleonardo@gmail.com>
2022-11-07 14:43:52 -0300

    day016 - oop

---
+++ day016/start/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+# object
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkGreen")
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+timmy.left(50)
+timmy.forward(100)
+
+
+print(timmy)
+
+# object attribute
+my_screen = Screen()
+print(my_screen.canvheight)
+
+
+# object method
+my_screen.exitonclick()
